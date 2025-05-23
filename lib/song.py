e5c4004342e1b3b9cd46b2953328ef3acabb7ca4
victoria655 <karonjivictoria@gmail.com>
2025-05-23 19:31:57 +0300

    added song.py

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,28 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        Song.count += 1
+
+        # Track genres
+        Song.genres.append(genre)
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+
+        # Track artists
+        Song.artists.append(artist)
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else:
+            Song.artist_count[artist] = 1
+
